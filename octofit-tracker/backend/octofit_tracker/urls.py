dbe44d2d5e345a85038859313ed5b3caa05a6506
Arsen Karapetyan <arsen.karapetyan@gmail.com>
2026-04-19 17:54:45 +0000

    docs: Add comment to urls.py explaining Codespace URL logic is handled in views.api_root, not urlpatterns

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -1,17 +1,11 @@
-"""octofit_tracker URL Configuration
+"""
+octofit_tracker URL Configuration
+
+NOTE: Codespace URL logic is handled in the API root view (views.api_root), not in urlpatterns.
+The API root dynamically builds URLs using the $CODESPACE_NAME environment variable for Codespace compatibility.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
